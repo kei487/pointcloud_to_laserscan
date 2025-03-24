1b80022bd9ced5d1c0f923e0a6f275197c47a272
Keitaro Nakamrua <numerugon487@gmail.com>
2025-03-24 19:44:28 +0900

    update launch

--- launch/mid360_pointcloud2_to_laserscan.launch.py
+++ launch/mid360_pointcloud2_to_laserscan.launch.py
@@ -3,6 +3,32 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+#    define_livox_frame = Node(
+#            package = 'tf2_ros',
+#            executable = 'static_transform_publisher',
+#            name = 'define_livox_frame',
+#            namespace = '',
+#            output = 'screen',
+#            arguments = [
+#                '--x', '0.1012', '--y', '0', '--z', '0.177',
+#                '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
+#                '--frame-id', 'base_link', '--child-frame-id', 'livox_frame',
+#                ],
+#            )
+
+#    define_robot_frame = Node(
+#            package = 'tf2_ros',
+#            executable = 'static_transform_publisher',
+#            name = 'define_robot_frame',
+#            namespace = '',
+#            output = 'screen',
+#            arguments = [
+#                '--x', '0.1012', '--y', '0', '--z', '0.177',
+#                '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
+#                '--frame-id', 'odom', '--child-frame-id', 'base_link',
+#                ],
+#            )
+
     pc2_to_scan = Node(
         package='pointcloud_to_laserscan',
         executable='pointcloud_to_laserscan_node',
@@ -10,12 +36,12 @@ def generate_launch_description():
         remappings=[('cloud_in', '/livox/lidar'),
                     ('scan', '/scan')],
         parameters=[{
-            'target_frame': 'lidar_link',
+            'target_frame': 'lidar_base',
             'transform_tolerance': 0.01,
-            'min_height': 0.3,
-            'max_height': 0.8,
-            'angle_min': -3.1415,
-            'angle_max': 3.1415,
+            'min_height': 0.0,
+            'max_height': 0.1,
+            'angle_min': -2.57, # -3.1415,
+            'angle_max': 2.57, # 3.1415,
             'angle_increment': 0.0087,  # M_PI/360.0
             'scan_time': 0.3333,
             'range_min': 0.3,
@@ -27,6 +53,8 @@ def generate_launch_description():
     )
 
     ld = LaunchDescription()
+#    ld.add_action(define_livox_frame)
+#    ld.add_action(define_robot_frame)
     ld.add_action(pc2_to_scan)
 
     return ld

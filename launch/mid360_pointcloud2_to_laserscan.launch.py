cc9f03814103d2adcd90d9d63c18f7bc428ec971
Keitaro Nakamrua <numerugon487@gmail.com>
2025-06-01 10:02:09 +0900

    more fix launch

--- launch/mid360_pointcloud2_to_laserscan.launch.py
+++ launch/mid360_pointcloud2_to_laserscan.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
 #                ],
 #            )
 
-    lidar_tf = actions.Node(
+    lidar_tf = Node(
 	name='lidar_tf',
 	package='tf2_ros',
 	executable='static_transform_publisher',

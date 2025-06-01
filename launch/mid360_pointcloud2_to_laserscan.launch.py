b57536d31f4ca1b5fa0dc54ffbfea39ae5be35cb
Keitaro Nakamrua <numerugon487@gmail.com>
2025-06-01 10:38:04 +0900

    change param

--- launch/mid360_pointcloud2_to_laserscan.launch.py
+++ launch/mid360_pointcloud2_to_laserscan.launch.py
@@ -51,8 +51,8 @@ def generate_launch_description():
         parameters=[{
             'target_frame': 'lidar_base',
             'transform_tolerance': 0.01,
-            'min_height': -0.4,
-            'max_height': 0.0,
+            'min_height': -0.3,
+            'max_height': 0.05,
             'angle_min': -2.50, # -3.1415,
             'angle_max': 2.50, # 3.1415,
             'angle_increment': 0.0087,  # M_PI/360.0
